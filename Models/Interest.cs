fbd4a6e0f0a67fa42a29899ad260ec4835a2d61e
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-03-16 12:02:17 -0800

    create model

---
+++ Models/Interest.cs
@@ -0,0 +1,10 @@
+﻿namespace MyResume.Models
+{
+    public class Interest
+    {
+        public int Id { get; set; }
+        public int ResumeId { get; set; }
+        public Resume Resume { get; set; }
+        public string InterestName { get; set; }
+    }
+}
